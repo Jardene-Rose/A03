cd5616f70fe1e7bdf9133a671c860375fdf8c4ac
Jardene-Rose <u3222008@uni.canberra.edu.au>
2023-11-03 22:48:17 +1100

    Final Tidy

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 let menuButtons = document.getElementsByClassName("btn"); //get btn elements//
 
 for (let i = 0; i < menuButtons.length; i++) { //goes through btn array//
+
     menuButtons[i].addEventListener("click", function() { //adds listener for mouse click//
 
         for (let j = 0; j < menuButtons.length; j++) { //resets buttons//
@@ -21,9 +22,13 @@ let menuBar = document.getElementById("menuBar"); //get menuBar element//
 let sticky = menuBar.offsetTop; //returns to top position of the element as a number?//
 
 document.addEventListener("scroll", function() { //adds listener to document//
+
     if (window.scrollY >= sticky) {
+
         menuBar.classList.add("sticky") //when scroll moves past initial element position- adds sticky class//
+
     } else {
+
         menuBar.classList.remove("sticky"); //when this is not the case, removes sticky class//
     }
 
@@ -32,19 +37,25 @@ document.addEventListener("scroll", function() { //adds listener to document//
 
 //Windows hidden to visible and reversed https://www.w3schools.com/css/css_display_visibility.asp//
 
-let windows = document.getElementsByClassName("windowPane");
-let visibleWindows = -1;
+let windows = document.getElementsByClassName("windowPane"); //get window array//
+let visibleWindows = -1; //-1 because array starts at 0//
 
 triangle01.addEventListener("click", function() { //how come this works without let X = document.getElementById("triangle01")//
-    if (visibleWindows > -1) {
-        windows[visibleWindows].classList.replace("visible", "hidden");
+
+    if (visibleWindows > -1) { //checks if windows shown are above 0//
+
+        windows[visibleWindows].classList.replace("visible", "hidden"); //swaps visible to hidden class//
+
         visibleWindows -= 1;
     }    
 });
 
 triangle02.addEventListener("click", function() {
-    if (visibleWindows < 4) {
+
+    if (visibleWindows < 4) { //checks if windows shown is above maximum//
+
         visibleWindows += 1;
-        windows[visibleWindows].classList.replace("hidden", "visible");
+        
+        windows[visibleWindows].classList.replace("hidden", "visible"); //swaps hidden to visible class//
     }    
 });
