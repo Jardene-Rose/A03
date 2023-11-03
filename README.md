8848955ef107d9d7ee429ca844c01f0598f5f8c1
Jardene-Rose <u3222008@uni.canberra.edu.au>
2023-11-03 21:41:34 +1100

    Adding Diagrams

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
+
 # A03
- 
+
+## Features
+
+### Active Button
+
+Menu with five buttons linked to each aspect of Le Corbusier's Five Points of Architecture. JS is for making button array, adding event listener for 'click', changes style to active button, and removes active from all other buttons. Hover is handled in CSS.
+
+### Sticky Menubar
+
+Menu moves to top of screen when scrolled past. JS adds an event listener 'scroll' to document, checks if the top of screen is past initial menu location, if so adds a class 'sticky' to menubar, otherwise removes it. CSS for 'sticky' to make the elements location fixed to the top of the screen.
+
+###  Strip Window Diagram
+
+Interactive diagram, left triangle button vanishes windows, right triangle button reveals them. JS adds listener 'click' to buttons, which then replaces 'visible' or 'hidden' classes respectively. CSS handle disappearing with Visability.
+
+## Challenges
+
+The largest challenge I encountered while undertaking A03 was balancing scope and time. As my first encounter with programming and web design, I was completely unable to estimate the length of a given task/problem. Something that I had budgeted a day for would take an hour, or a week, and I had no way of knowing prior.
+
+The best solution for this would have been to frequently ask for help in tutorials, which I did not do. I think programming may be too similar to puzzle solving for my brain, meaning I obstinately fixate on finding the solution by myself.
+
+## Achievements
+
+Though I didn't accomplish a high degree of complexity with the website, I do believe I developed a good foundation. I now have something to build upon for future projects.
+
+## Improvements
+
+I think I could improve in user feedback, and engagement. The site is interesting to me due to the topic and diagrams, but I don't think this would be enough for a general user.
+
+## Sources
+
+ 1. Active Button (https://www.w3schools.com/howto/howto_js_active_element.asp)
+ 2. Sticky Menu (https://www.w3schools.com/jsref/prop_win_scrolly.asp)
+ 3. Display/Visibility (https://www.w3schools.com/css/css_display_visibility.asp)
--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 
 
-//Menu Hover https://www.w3schools.com/howto/howto_js_active_element.asp//
+//Menu Hover and Clicked https://www.w3schools.com/howto/howto_js_active_element.asp//
 
-let menuBar = document.getElementById("menuBar");
+let menuButtons = document.getElementsByClassName("btn"); //get btn elements//
 
-let menuButtons = menuBar.getElementsByClassName("btn");
+for (let i = 0; i < menuButtons.length; i++) { //goes through btn array//
+    menuButtons[i].addEventListener("click", function() { //adds listener for mouse click//
 
-for (let i = 0; i < menuButtons.length; i++) {
-    menuButtons[i].addEventListener("onclick", function clicked() {
-
-        let current = document.getElementsByClassName("active");
-
-        if (current.length > 0) {
-            current[0].className = current[0].className.replace("active", "");
+        for (let j = 0; j < menuButtons.length; j++) { //resets buttons//
+            menuButtons[j].className = "btn";
         }
-        
-        this.className += "active";
+
+        menuButtons[i].className = "active btn"; //designating active button//
     });
 }
 
+//Menu Sticky https://www.w3schools.com/jsref/prop_win_scrolly.asp//
 
-//Menu Sticky//
+let menuBar = document.getElementById("menuBar"); //get menuBar element//
+let sticky = menuBar.offsetTop; //returns to top position of the element as a number?//
 
-//Smooth Scroll//
+document.addEventListener("scroll", function() { //adds listener to document//
+    if (window.scrollY >= sticky) {
+        menuBar.classList.add("sticky") //when scroll moves past initial element position- adds sticky class//
+    } else {
+        menuBar.classList.remove("sticky"); //when this is not the case, removes sticky class//
+    }
 
+    console.log(scrollY);
+});
+
+//Windows hidden to visible and reversed https://www.w3schools.com/css/css_display_visibility.asp//
+
+let windows = document.getElementsByClassName("windowPane");
+let visibleWindows = -1;
+
+triangle01.addEventListener("click", function() { //how come this works without let X = document.getElementById("triangle01")//
+    if (visibleWindows > -1) {
+        windows[visibleWindows].classList.replace("visible", "hidden");
+        visibleWindows -= 1;
+    }    
+});
+
+triangle02.addEventListener("click", function() {
+    if (visibleWindows < 4) {
+        visibleWindows += 1;
+        windows[visibleWindows].classList.replace("hidden", "visible");
+    }    
+});
